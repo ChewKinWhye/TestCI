c9ca1808747ed42730fca5be3632fd650a0b06f3
ChewKinWhye <Ckinwhye@hotmail.com>
2020-05-18 17:25:45 +0800

    Travis CI

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,17 @@
 import calculator
 
 class TestCalculator:
+    def setup_class(self):
+        print("setup_class called once for the class")
+
+    def teardown_class(self):
+        print("teardown_class called once for the class")
+
+    def setup_method(self):
+        print("setup_method called for every method")
+
+    def teardown_method(self):
+        print("teardown_method called for every method")
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
